5cf8564f9e17f743a202181cbd32021d8868b33c
Jijica <129999333+Jijica@users.noreply.github.com>
2023-09-19 16:02:48 +0300

    release

---
+++ Files management/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Files_management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"c:\Otus\TestDir1",
+                path2 = @"c:\Otus\TestDir2";
+            FilesHandler.CreateDirectory(path1);
+            FilesHandler.CreateDirectory(path2);
+            FilesHandler.CreateFiles(path1, 10);
+            FilesHandler.CreateFiles(path2, 10);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(true);
+            FilesHandler.FileWriteName(path1);
+            FilesHandler.FileWriteName(path2);
+            FilesHandler.FileWriteDateTime(path1);
+            FilesHandler.FileWriteDateTime(path2);
+            FilesHandler.ReadFiles(path1);
+            FilesHandler.ReadFiles(path2);
+            Console.WriteLine("Press any key to delete directories and exit");
+            Console.ReadKey(true);
+            FilesHandler.DeleteDirectory(@"c:\Otus");
+        }
+    }
+}
