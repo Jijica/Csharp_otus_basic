5d5d0bc243155860c1f50a8962ed11a12023a7a8
Jijica <129999333+Jijica@users.noreply.github.com>
2023-09-23 00:46:13 +0300

    fix

--- LINQ method/Program.cs
+++ LINQ method/Program.cs
@@ -5,16 +5,13 @@
         static void Main(string[] args)
         {
             var collection = new List<int>();
-            var test = new List<int> { 1213,12312,423423,234234};
-            var tested = test.OrderDescending().ToList();
             var rnd = new Random();
             for (int i = 0; i < 20; i++)
             {
                 collection.Add(rnd.Next(1, 100000));
             }
             var topped = collection.Top(30, e => e - 1000000).ToList();
-           // topped.ForEach(Console.WriteLine);
-            tested.ForEach(Console.WriteLine);
+            topped.ForEach(Console.WriteLine);
         }
     }
 }
